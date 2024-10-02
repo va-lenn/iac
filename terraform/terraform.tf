65ee82d36162d60f054041bee7cdeb4208d61932
va-lenn <info.muhammadvalen@gmail.com>
2024-10-02 15:33:59 +0700

    initial commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "tfgitops"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,13 +44,13 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-0866a3c8686eaeeba" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "t3.medium"
 }
 
 variable "key_name" {
